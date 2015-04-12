d0d04935ca68166914f7b4cdda65bc4a067c640f
Jianyang Zhang <jianyz@uw.edu>
2015-04-12 07:54:53 -0700

    added websocket

--- static/js/3d/main.js
+++ static/js/3d/main.js
@@ -915,4 +915,23 @@
   me.getAddedObjects = function() {
     return added_objects;
   };
+  };
+  
+  // called by clicking launch????????????????//
+  function socket(){
+    var connection = new WebSocket('url??');
+    connection.onopen = function () {
+      connection.send(speed + " " + height + " " + angle); //????????????/
+    } ;
+    connection.onmessage = function (evt) {
+      //console.log("Got ws message: " + evt.data); //??????????????
+      var data = evt.data;
+      if(data.type === "add"){
+        addOrbit(data.name, evt.data);
+      } else if(data.type === "remove"){
+        deleteOrbit(data.name);
+      } else {
+          mordifyOrbit(data.name, data);
+      }
+    };
 }
